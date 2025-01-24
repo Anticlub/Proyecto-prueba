00a883042429634ebfc48b9f8c264d4df75ab4d1
Cristofer <cristofdzfdz@gmail.com>
2025-01-24 10:51:46 +0100

    version antes del checkout

--- src/App.java
+++ src/App.java
@@ -4,5 +4,6 @@ public class App {
         Persona p2 = new Persona("Luis");
         System.out.println(p2.getNombre());
         Persona p3 = new Persona("Antonio");
+        System.out.println("Mensaje para probrar el checkout del ejercicio de git");
     }
 }
