8369c8e1c1ea9390303cad0d208c48ad6c4f1200
Cristofer <cristofdzfdz@gmail.com>
2025-01-24 10:39:23 +0100

    ampliacion de app.java

--- src/App.java
+++ src/App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         Persona p1 = new Persona("aaaa");
         System.out.println(p1.getNombre());
+        Persona p2 = new Persona("Luis");
+        System.out.println(p2.getNombre());
     }
 }
